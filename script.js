f322f84646943d0bb86a0ea13fcfdab786ce1f96
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-02-18 13:34:26 -0300

    Fórmula para calcular a idade

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function calcAge() {
     var currday = currtime.getDate();
     var currmonth = currtime.getMonth();
     var curryear = currtime.getFullYear();
-    var  months31days = [1, 3, 5, 7, 8, 10, 12];
+    //var months31days = [1, 3, 5, 7, 8, 10, 12];
+    var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
     //Validando os inputs
     var invday = false;
@@ -23,6 +24,11 @@ function calcAge() {
         var bmonth = Number(bmonthtxt.value);
         var byear = Number(byeartxt.value);
     
+        //No caso de anos bissextos
+        if (curryear % 4 == 0) {
+            months[1] = 29;
+        }
+
         if (bday < 1 || bday > 31) {
             //window.alert('Insira um dia válido');
             invday = true;
@@ -31,12 +37,7 @@ function calcAge() {
         if (bmonth < 1 || bmonth > 12) {
             //window.alert('Insira um mês válido');
             invmonth = true;
-        } else if (months31days.indexOf(bmonth) == -1 && bday == 31) {
-            //window.alert('O dia inserido não existe');
-            invday = true;
-        }
-
-        if (bmonth == 2 && bday > 28) {
+        } else if (bday > months[bmonth-1]) {
             //window.alert('O dia inserido não existe');
             invday = true;
         }
@@ -45,7 +46,7 @@ function calcAge() {
             invyear = true;
         }
         
-        if ((byear == curryear) && (bday >= currday || bmonth > currmonth)){
+        if ((byear == curryear) && (bday > currday || bmonth > currmonth)){
             //window.alert('A data inserida está no futuro');
             invyear = true;
         }
@@ -57,8 +58,39 @@ function calcAge() {
             
         //Checando se todos são válidos
         if (invday == false && invmonth == false && invyear == false) {
-            window.alert('Tudo ok')
+
+            var age = {
+                days: undefined,
+                months: undefined,
+                years: undefined
+            };
+            age.years = curryear - byear;
+            
+            let prevmonth = currmonth-1;
+            
+            if (prevmonth < 0) {
+                prevmonth = 12;
+            }
+            if (currday < bday) {
+                age.months--;
+                currday += months[prevmonth];
+            }
+            age.days = currday - bday;
+
+            if (currmonth < bmonth) {
+                currmonth += 12;
+                age.years--;
+            } 
+            age.months = currmonth - bmonth;
+
         }
+
+        var rday = document.getElementById('rday');
+        rday.innerText = age.days;
+        var rmonth = document.getElementById('rmonth');
+        rmonth.innerText = age.months;
+        var ryear = document.getElementById('ryear');
+        ryear.innerText = age.years;
     }
 }
 
